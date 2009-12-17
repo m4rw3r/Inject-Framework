35b71652bb4ba65a92c9ad9b1f7dacf2b0367a98
m4rw3r <m4rw3r@gmail.com>
2009-12-17 21:10:33 +0100

    Moved the addition of loggers and dispatcher to app/config/inject.php

--- app/config/inject.php
+++ app/config/inject.php
@@ -1,13 +1,59 @@
 <?php
 
-// Add a logger
+/*
+ * Create and add loggers.
+ * 
+ * Inject uses an observer-like variant for the loggers:
+ * 
+ * You create a logger object which is responsible for storing
+ * or printing the log messages somewhere.
+ * Then this logger is passed to Inject::attachLogger(logger_object, [level = ALL]).
+ * The second parameter is an error constant (from the Inject class)
+ * which adjusts which errors this logger should log.
+ */
+
+// Add a logger which prints all messages to the screen
 Inject::attachLogger(new Inject_Logger_Screenwriter("\n"));
 
-// Use the standard dispatcher
-Inject::setDispatcher(new Inject_Dispatcher);
+// Add a file logger for ERROR and WARNING, saves the files in log.txt
+Inject::attachLogger(new Inject_Logger_File('log.txt'), Inject::ERROR | Inject::WARNING);
 
-// Autoload some instances (ie. set global instances
-// Inject_Registry::setGlobal('database', new Db);
+
+
+
+/*
+ * Create the standard dispatcher object.
+ *
+ * The dispatcher is the object which is responsible for instantiating
+ * and calling the class->method it receives from the request object.
+ * It is also responsible for any default class/method to run.
+ */
+$d = new Inject_Dispatcher();
+
+// set defaults
+$d->setDefaultControllerClass('Controller_Welcome');
+$d->setDefaultControllerAction('index');
+
+// Tell Inject to use the configured dispatcher
+Inject::setDispatcher($d);
+
+
+
+
+/*
+ * Set global instances.
+ * 
+ * All the instances assigned using Inject_Registry::setGlobal() will be
+ * available to all children of Inject_Registry. This also includes all
+ * Inject_Request objects.
+ * 
+ * The globals can also locally (in an object instance) be overridden,
+ * so that a specific instance uses other values.
+ */
+//Inject_Registry::setGlobal('database', new Db);
+
+
+
 
 // Not implemented settings:
 
--- index.php
+++ index.php
@@ -13,19 +13,9 @@ require './lib/Inject.php';
 // Add the autoloader and error handling
 Inject::init();
 
-Inject::attachLogger(new Inject_Logger_Screenwriter("\n"));
+Inject::addPaths(array('app'));
 
-Inject::setDispatcher(new Inject_Dispatcher);
-
-Inject::addPath('app');
-
-//Inject::attach_logger(new Inject_Logger_File(dirname(__FILE__) . '/log.txt'));
-
-/*echo "<pre>";
-echo new URL('post/show', array('year' => 2008, 'month' => 34, 'day' => 4));*/
-
-
-Inject::run(new Inject_Request_HTTP);
+Inject::run(new Inject_Request_HMVC('Controller_Test', 'test_action'));
 
 /* End of file index.php */
 /* Location: . */
