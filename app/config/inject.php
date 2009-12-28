3fff552a45e63a2920fa06fe4e51c3e102e3f0bb
m4rw3r <m4rw3r@gmail.com>
2009-12-28 02:22:58 +0100

    Added settings for error handling

--- app/config/inject.php
+++ app/config/inject.php
@@ -1,6 +1,46 @@
 <?php
 
 /*
+ * Create the standard dispatcher object.
+ *
+ * The dispatcher is the object which is responsible for instantiating
+ * and calling the class->method it receives from the request object.
+ * It is also responsible for any default class/method to run.
+ */
+
+// Use the default dispatcher
+$d = new Inject_Dispatcher();
+
+// Set default controller and action
+$d->setDefaultHandler('Controller_Welcome', 'index');
+
+// Set the error handlers in case something goes wrong
+$d->set404Handler('Controller_Welcome', 'error');
+
+// Tell Inject to use the configured dispatcher
+Inject::setDispatcher($d);
+
+
+
+
+/*
+ * Set error handling settings.
+ * 
+ * The Inject class also has settings for error handling, if the error should
+ * be shown to the user or if just a static page should be shown and which
+ * error types that Inject should abort execution on.
+ */
+
+// Set error on which execution sholud be aborted
+Inject::setErrorLevel(Inject::ERROR | Inject::WARNING);
+
+// Determines if the user should see the errors, false is yes
+Inject::setProduction(false);
+
+
+
+
+/*
  * Create and add loggers.
  * 
  * Inject uses an observer-like variant for the loggers:
@@ -17,27 +57,6 @@ Inject::attachLogger(new Inject_Logger_Screenwriter("\n"));
 
 // Add a file logger for ERROR and WARNING, saves the files in log.txt
 Inject::attachLogger(new Inject_Logger_File('/Users/m4rw3r/Sites/Inject-Framework/log.txt'), Inject::ERROR | Inject::WARNING);
-
-
-
-
-/*
- * Create the standard dispatcher object.
- *
- * The dispatcher is the object which is responsible for instantiating
- * and calling the class->method it receives from the request object.
- * It is also responsible for any default class/method to run.
- */
-$d = new Inject_Dispatcher();
-
-// Set default controller and action
-$d->setDefaultHandler('Controller_Welcome', 'index');
-
-// set the error handlers in case something goes wrong
-$d->set404Handler('Controller_Welcome', 'error');
-
-// Tell Inject to use the configured dispatcher
-Inject::setDispatcher($d);
 
 
 
