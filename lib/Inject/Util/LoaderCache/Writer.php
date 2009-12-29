14ccf29334d432539b7ebb90b30eb709bf58431f
m4rw3r <m4rw3r@gmail.com>
2009-12-29 02:18:43 +0100

    Added namespace support to the Inject_Util_LoaderCache_Writer

--- lib/Inject/Util/LoaderCache/Writer.php
+++ lib/Inject/Util/LoaderCache/Writer.php
@@ -10,11 +10,33 @@
  */
 class Inject_Util_LoaderCache_Writer
 {
+	/**
+	 * The regex determining which files to search.
+	 * 
+	 * @var string
+	 */
 	protected $file_regex = '/\.php$/';
 	
+	/**
+	 * A list of paths to search, the most important last.
+	 * 
+	 * @var array
+	 */
 	protected $paths = array();
 	
+	/**
+	 * The list of found classes, class => file.
+	 * 
+	 * @var array
+	 */
 	protected $list = array();
+	
+	/**
+	 * The token constant for T_NAMESPACE, filled with a dummy value for PHP < 5.3.
+	 * 
+	 * @var int
+	 */
+	protected $namespace_token = false;
 	
 	// ------------------------------------------------------------------------
 
@@ -26,6 +48,10 @@ class Inject_Util_LoaderCache_Writer
 	public function __construct(array $paths)
 	{
 		$this->paths = $paths;
+		
+		// Use constant() to prevent compiler errors, if not PHP > 5.3 use a random
+		// (huge) number to prevent errors:
+		$this->namespace_token = version_compare(PHP_VERSION, '5.3', '>=') ? constant('T_NAMESPACE') : 476389246;
 	}
 	
 	// ------------------------------------------------------------------------
@@ -33,14 +59,15 @@ class Inject_Util_LoaderCache_Writer
 	/**
 	 * Scans the directories for classes and constructs an internal array.
 	 * 
-	 * @return int
+	 * @return int		The number of found classes
 	 */
 	function scan()
 	{
 		foreach($this->paths as $path)
 		{
 			$len = strlen($path);
-			$files = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($path), RecursiveIteratorIterator::SELF_FIRST);
+			
+			$files = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($path));
 			
 			foreach($files as $name => $file)
 			{
@@ -79,16 +106,32 @@ class Inject_Util_LoaderCache_Writer
 	 */
 	protected function getClasses($filepath)
 	{
+		$tokens = token_get_all(file_get_contents($filepath));
+		
 		$classes = array();
-		$code = file_get_contents($filepath);
-		$tokens = token_get_all($code);
 		$is_classname = false;
+		$is_namespace = false;
+		$inside_namespace = false;
+		$indentation = 0;
+		$current_ns = null;
 		
 		foreach($tokens as $token)
 		{
 			if( ! is_array($token))
 			{
+				switch($token)
+				{
+					case '{':
+						$indentation++;
+						break;
+						
+					case '}':
+						$indentation--;
+						break;
+				}
+				
 				$is_classname = false;
+				$is_namespace = false;
 				
 				continue;
 			}
@@ -97,16 +140,35 @@ class Inject_Util_LoaderCache_Writer
 			{
 				case T_WHITESPACE:
 					break;
+					
 				case T_CLASS:
+				case T_INTERFACE:
 					$is_classname = true;
 					break;
+					
+				case $this->namespace_token:
+					$is_namespace = true;
+					$inside_namespace = true;
+					// reset so we're sure that we get an empty namespace if the user decides
+					// to create one ("namespace;"):
+					$current_ns = '';
+					break;
+					
 				case T_STRING:
 					if($is_classname)
 					{
-						$classes[] = $token[1];
+						// Found a class, add the namespace if we have one (which isn't the global, "empty" namespace)
+						$classes[] = ($inside_namespace && ! empty($current_ns) ? '\\'.$current_ns.'\\' : '').$token[1];
 					}
+					// namespaces cannot be within indentation
+					elseif($is_namespace && $indentation == 0)
+					{
+						$current_ns = $token[1];
+					}
+					
 				default:
 					$is_classname = false;
+					$is_namespace = false;
 			}
 		}
 		
@@ -115,5 +177,5 @@ class Inject_Util_LoaderCache_Writer
 }
 
 
-/* End of file URI.php */
-/* Location: ./Users/m4rw3r/Sites/Inject-Framework/lib/Inject/URI.php */
+/* End of file Writer.php */
+/* Location: ./lib/Inject/Utils/LoaderWriter */
