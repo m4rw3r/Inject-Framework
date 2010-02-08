f1d0689430cb4da6761182e2548e5373263d6388
m4rw3r <m4rw3r@gmail.com>
2010-02-08 22:00:48 +0100

    Added basic view renderer

--- lib/Inject/Controller.php
+++ lib/Inject/Controller.php
@@ -40,6 +40,59 @@ class Inject_Controller
 	{
 		return $this->request->getService($prop);
 	}
+	
+	// ------------------------------------------------------------------------
+
+	/**
+	 * Renders a view using the default template renderer which uses PHP to process
+	 * the templates.
+	 * 
+	 * @param  string	The path to the file, relative to the Views folder, except file extension
+	 * @param  array	The data to render, will be extracted to variables in the view
+	 * @param  bool		If to return the rendered content to the caller
+	 * @param  string	The file extension of the file to render
+	 * @return string|void
+	 */
+	public function render($view_name, $data = array(), $return_rendered = false, $filetype = '.php')
+	{
+		$__found_file = false;
+		
+		extract($data);
+		
+		ob_start();
+		
+		// Find the view
+		foreach(Inject::getApplicationPaths() as $p)
+		{
+			if(file_exists($p.'Views/'.$view_name.$filetype))
+			{
+				$__found_file = true;
+				
+				include $p.'Views/'.$view_name.$filetype;
+				
+				break;
+			}
+		}
+		
+		// get content
+		$buffer = ob_get_contents();
+		ob_end_clean();
+		
+		if( ! $__found_file)
+		{
+			// Replace with appropriate exception
+			throw new Exception('Cannot find view "'.$view_name.$filetype.'"');
+		}
+		
+		if($return_rendered)
+		{
+			return $buffer;
+		}
+		else
+		{
+			$this->request->response = $buffer;
+		}
+	}
 }
 
 /* End of file Controller.php */
