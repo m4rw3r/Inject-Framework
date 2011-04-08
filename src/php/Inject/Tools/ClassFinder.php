962ea869318375d4322ae02d8f688c3e7b5aedf8
m4rw3r <m4rw3r@gmail.com>
2011-04-08 21:09:04 +0200

    Tools\ClassFinder: Improved it greatly, does no longer search the same path twice, now raises notices upon discovering duplicate classes

--- src/php/Inject/Tools/ClassFinder.php
+++ src/php/Inject/Tools/ClassFinder.php
@@ -9,10 +9,36 @@ namespace Inject\Tools;
 
 /**
  * Searches trough a set of paths for classes, returns a list of classes
- * and in which file they are located.
+ * and in which file they are located, does NOT include the files.
+ * 
+ * Raises a PHP NOTICE if duplicate class identifiers are found.
  */
 class ClassFinder
 {
+	/**
+	 * Tells the ClassFinder to raise a PHP NOTICE error when a duplicate class
+	 * is found.
+	 * 
+	 * @var int
+	 */
+	const TRIGGER_NOTICE_ON_DUPLICATE = 1;
+	
+	/**
+	 * Tells the ClassFinder to raise a PHP WARNING error when a duplicate class
+	 * is found.
+	 * 
+	 * @var int
+	 */
+	const TRIGGER_WARNING_ON_DUPLICATE = 2;
+	
+	/**
+	 * Tells the ClassFinder to raise a PHP ERROR error when a duplicate class
+	 * is found.
+	 * 
+	 * @var int
+	 */
+	const TRIGGER_ERROR_ON_DUPLICATE = 4;
+	
 	/**
 	 * The list containing classes and their filenames.
 	 * 
@@ -28,14 +54,38 @@ class ClassFinder
 	protected $file_regex;
 	
 	/**
-	 * A list of paths to search
+	 * A list of paths to search.
+	 * 
+	 * @var array(string)
 	 */
 	protected $paths = array();
 	
-	function __construct($paths = '.', $file_regex = '/\.php$/')
+	/**
+	 * The operation flags for this ClassFinder, consists of binary flags
+	 * from class constants.
+	 * 
+	 * @var int
+	 */
+	protected $flags = self::TRIGGER_NOTICE_ON_DUPLICATE;
+	
+	// ------------------------------------------------------------------------
+	
+	/**
+	 * @param  string|array
+	 * @param  string
+	 * @param  int
+	 */
+	function __construct($paths = '.', $file_regex = '/\.php$/', $flags = self::TRIGGER_NOTICE_ON_DUPLICATE)
 	{
-		$this->paths = (Array) $paths;
+		foreach((Array) $paths as $p)
+		{
+			$this->paths[] = realpath($p);
+		}
+		
+		// array_unique() will fix problems with eg. PHP's include path
+		$this->paths      = array_unique($this->paths);
 		$this->file_regex = $file_regex;
+		$this->flags      = $flags;
 	}
 	
 	// ------------------------------------------------------------------------
@@ -55,19 +105,34 @@ class ClassFinder
 		
 		foreach($this->paths as $path)
 		{
-			$path = realpath($path);
-			
 			// Search the folder
-			$files = new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($path));
+			$diriter  = new \RecursiveDirectoryIterator($path);
+			$iteriter = new \RecursiveIteratorIterator($diriter, \RecursiveIteratorIterator::LEAVES_ONLY);
+			$files    = new \RegexIterator($iteriter, $this->file_regex);
 			
 			foreach($files as $name => $file)
 			{
-				if(preg_match($this->file_regex, $name))
+				foreach($this->getClasses($name) as $class)
 				{
-					foreach($this->getClasses($name) as $class)
+					if(isset($this->list[$class]))
 					{
-						$this->list[$class] = $name;
+						$msg = sprintf('ClassFinder: Conflicting class name found: %s in file %s', $class, $name);
+						
+						if($this->flags & self::TRIGGER_ERROR_ON_DUPLICATE)
+						{
+							\trigger_error($msg, \E_ERROR_NOTICE);
+						}
+						elseif($this->flags & self::TRIGGER_WARNING_ON_DUPLICATE)
+						{
+							\trigger_error($msg, \E_USER_WARNING);
+						}
+						elseif($this->flags & self::TRIGGER_NOTICE_ON_DUPLICATE)
+						{
+							\trigger_error($msg, \E_USER_NOTICE);
+						}
 					}
+					
+					$this->list[$class] = $name;
 				}
 			}
 		}
@@ -80,19 +145,19 @@ class ClassFinder
 	/**
 	 * Tokenizes the file and iterates all tokens in search of classes.
 	 * 
-	 * @param  string
-	 * @return array
+	 * @param  string         Path to file
+	 * @return array(string)  List of classnames
 	 */
 	protected function getClasses($filepath)
 	{
 		$tokens = token_get_all(file_get_contents($filepath));
 		
-		$classes = array();
-		$is_classname = false;
-		$is_namespace = false;
+		$classes          = array();
+		$is_classname     = false;
+		$is_namespace     = false;
 		$inside_namespace = false;
-		$indentation = 0;
-		$current_ns = null;
+		$indentation      = 0;
+		$current_ns       = null;
 		
 		foreach($tokens as $token)
 		{
@@ -151,6 +216,8 @@ class ClassFinder
 						$current_ns .= $token[1];
 						continue;
 					}
+					// Intentionally no break
+					
 				case T_NS_SEPARATOR:
 					// Allow multple levels of namespaces
 					if($is_namespace && $indentation == 0)
@@ -158,6 +225,8 @@ class ClassFinder
 						$current_ns .= $token[1];
 						continue;
 					}
+					// Intentionally no break
+					
 				default:
 					// Something else, not a namespace or class
 					$is_classname = false;
